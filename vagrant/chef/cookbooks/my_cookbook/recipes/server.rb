2455dcf83df5207b57687a61be743b3ef681f3b4
Phil Brown <phil.brown@open.edu.au>
2013-03-05 16:51:41 +1100

    Add composer install task

--- vagrant/chef/cookbooks/my_cookbook/recipes/server.rb
+++ vagrant/chef/cookbooks/my_cookbook/recipes/server.rb
@@ -14,6 +14,17 @@ end
 
 package "zend-server-php-5.4"
 package "apache2-mpm-itk"
+package "curl"
+
+execute "install composer" do
+  command "curl -sS https://getcomposer.org/installer | /usr/local/zend/bin/php -- --install-dir=/usr/local/bin"
+end
+
+execute "create composer symlink" do
+  command "ln -s /usr/local/bin/composer.phar /usr/local/bin/composer"
+  only_if "test -f /usr/local/bin/composer.phar"
+  not_if "test -L /usr/local/bin/composer"
+end
 
 cookbook_file "/etc/profile.d/zend-server.sh" do
   source "zend-server.sh"
